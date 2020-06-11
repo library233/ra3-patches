b61a470fb30fdcdda8c2cdf39d3f89c09ffa988d
NVXARM <nvxarm@foxmail.com>
2020-06-11 08:02:16 +0800

    update

--- language/chinese-src/build.cmd
+++ language/chinese-src/build.cmd
@@ -1,5 +1,6 @@
 @echo off
 cd /d "%~dp0"
+rmdir /s /q build 2>nul
 bin\makebig.exe direction "res\red alert 3" -o:"build\red alert 3\data\lang-english11.big"
 bin\makebig.exe direction "res\red alert 3 uprising" -o:"build\red alert 3 uprising\data\english.big"
 pause
