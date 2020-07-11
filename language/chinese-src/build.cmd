4886a1c79270a8de797500013bcfc7adb6b4a2de
NVXARM <nvxarm@foxmail.com>
2020-07-11 15:44:10 +0800

    update

--- language/chinese-src/build.cmd
+++ language/chinese-src/build.cmd
@@ -1,6 +1,11 @@
 @echo off
 cd /d "%~dp0"
 rmdir /s /q build 2>nul
-bin\makebig.exe direction "res\red alert 3" -o:"build\red alert 3\data\lang-english11.big"
-bin\makebig.exe direction "res\red alert 3 uprising" -o:"build\red alert 3 uprising\data\english.big"
+robocopy /e res build\tmp > nul
+cd build
+robocopy /e tmp\fonts "tmp\red alert 3\data\fonts" > nul
+robocopy /e tmp\fonts "tmp\red alert 3 uprising\data\fonts" > nul
+..\bin\makebig.exe direction "tmp\red alert 3" -o:"red alert 3\data\lang-english11.big"
+..\bin\makebig.exe direction "tmp\red alert 3 uprising" -o:"red alert 3 uprising\data\english.big"
+rmdir /s /q tmp
 pause
